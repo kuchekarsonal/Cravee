397a1890146408cfab36cca53c2b9585df735fe2
kuchekarsonal <41698085+kuchekarsonal@users.noreply.github.com>
2024-10-25 15:24:01 +0530

    backend
    backend

---
+++ backend/craveeApp/src/main/java/com/sona/craveeApp/CraveeAppApplication.java
@@ -0,0 +1,13 @@
+package com.sona.craveeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraveeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraveeAppApplication.class, args);
+	}
+
+}
---
+++ backend/craveeApp/src/test/java/com/sona/craveeApp/CraveeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sona.craveeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraveeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
